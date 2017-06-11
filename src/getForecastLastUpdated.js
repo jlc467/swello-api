053c9a4c3c03961ee40ef9e6f9274804509e5d50
John Collins <jlc467@users.noreply.github.com>
2017-06-11 18:00:23 -0400

    graph.cool

--- src/getForecastLastUpdated.js
+++ src/getForecastLastUpdated.js
@@ -1,8 +1,8 @@
 const chrono = require('chrono-node');
 
-function getLastUpdated($) {
+function getForecastLastUpdated($) {
   const lastUpdated = $('.marine-update-notice').text().replace('Last Update:', '').trim();
   return chrono.parseDate(lastUpdated);
 }
 
-module.exports = getLastUpdated;
+module.exports = getForecastLastUpdated;
